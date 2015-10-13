59b8c46a61fb0a15a6084af15fc3417c21ae1ec2
Daniel Okwufulueze <daniel@dealdey.com>
2015-10-13 23:24:32 +0100

    Add faqs

--- js/buyers_guide.js
+++ js/buyers_guide.js
@@ -15,12 +15,25 @@ BuyersGuide.prototype._addEventListenerToBody = function() {
   this._$body.on('mouseover', function(eventObject) {
     var $target = $(eventObject.target);
     if ($target.is('div.icon')) {
-      $('.'+$target.attr('id')).delay(300).slideDown(800);
+      if ($('.'+$target.attr('id')).get(0).style.display == 'none') { 
+        $('.'+$target.attr('id')).delay(300).slideDown(800, function() {
+          var body = document.body || document.documentElement;
+          // body.scrollTop = body.scrollHeight;
+        });
+      }
     }
   }.bind(this)).on('mouseout', function(eventObject) {
     var $target = $(eventObject.target);
     if ($target.is('div.icon')) {
       $('.'+$target.attr('id')).slideUp(250);
+    }
+  }.bind(this)).on('click', function(eventObject) {
+    var $target = $(eventObject.target);
+    if ($target.is('.accord-heading') || $target.is('.accord-heading > span')) {
+      $('.active').not($target.closest('h6')).removeClass('active');
+      $target.closest('h6').toggleClass('active');
+      $('.accord-data').not($target.closest('h6').next()).slideUp(500);
+      $target.closest('h6').next().slideToggle(500);
     }
   }.bind(this));
 }
