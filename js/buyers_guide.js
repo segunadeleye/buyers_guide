4a6cf96d496b5962a062056f259c474eb8e5c5ed
Daniel Okwufulueze <daniel@dealdey.com>
2015-10-15 16:53:09 +0100

    Add new features: Save on shipping!

--- js/buyers_guide.js
+++ js/buyers_guide.js
@@ -12,20 +12,25 @@ BuyersGuide.prototype._init = function() {
 }
 
 BuyersGuide.prototype._addEventListenerToBody = function() {
+  timeout = null;
+  
   this._$body.on('mouseover', function(eventObject) {
     var $target = $(eventObject.target);
-    if ($target.is('div.icon')) {
-      if ($('.'+$target.attr('id')).get(0).style.display == 'none') { 
-        $('.'+$target.attr('id')).delay(300).slideDown(800, function() {
-          var body = document.body || document.documentElement;
-          // body.scrollTop = body.scrollHeight;
-        });
+    if ($target.is('div.icon') || $target.is('div.icon p')) {
+      if ($('.'+$target.closest('div').attr('id')).get(0).style.display == 'none') { 
+        timeout = setTimeout(function() {
+          $('.'+$target.closest('div').attr('id')).stop(true, true).slideDown(350, function() {
+            var body = document.body || document.documentElement;
+            // body.scrollTop = body.scrollHeight;
+          });
+        }, 600);
       }
     }
   }.bind(this)).on('mouseout', function(eventObject) {
     var $target = $(eventObject.target);
-    if ($target.is('div.icon')) {
-      $('.'+$target.attr('id')).slideUp(250);
+    if ($target.is('div.icon') || $target.is('div.icon p')) {
+      clearTimeout(timeout);
+      $('.'+$target.closest('div').attr('id')).stop(true, true).slideUp('fast');
     }
   }.bind(this)).on('click', function(eventObject) {
     var $target = $(eventObject.target);
